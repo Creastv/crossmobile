e92fbcaa906985c97c40f1ae2867118773daa3f8
Creastv <creastv@gail.com>
2024-10-28 14:21:18 +0100

    offert

--- gutenberg/topup-boxes/index.php
+++ gutenberg/topup-boxes/index.php
@@ -159,29 +159,32 @@ require dirname(__FILE__, 2) . '/defaults.php';
 <?php }; ?>
 
 <script>
-    function setEqualHeight() {
-        let maxHeight = 0;
+    $(function() {
+        function setEqualHeight() {
+            let maxHeight = 0;
 
-        // Resetujemy wysokości, aby obliczenia były poprawne po zmianie rozmiaru okna
-        $('.features-2 .single-offer').css('height', 'auto');
+            // Resetujemy wysokości, aby obliczenia były poprawne po zmianie rozmiaru okna
+            $('.features-2 .single-offer').css('height', 'auto');
 
-        // Znajdź najwyższy element
-        $('.features-2 .single-offer').each(function() {
-            let thisHeight = $(this).outerHeight();
-            if (thisHeight > maxHeight) {
-                maxHeight = thisHeight;
-            }
+            // Znajdź najwyższy element
+            $('.features-2 .single-offer').each(function() {
+                let thisHeight = $(this).outerHeight();
+                if (thisHeight > maxHeight) {
+                    maxHeight = thisHeight;
+                }
+            });
+
+            // Ustaw każdemu elementowi najwyższą wysokość
+            $('.features-2 .single-offer').css('height', maxHeight + 'px');
+        }
+
+        // Uruchom funkcję po załadowaniu strony
+        setEqualHeight();
+
+        // Uruchom funkcję również przy zmianie rozmiaru okna
+        $(window).on('resize', function() {
+            setEqualHeight();
         });
-
-        // Ustaw każdemu elementowi najwyższą wysokość
-        $('.features-2 .single-offer').css('height', maxHeight + 'px');
-    }
-
-    // Uruchom funkcję po załadowaniu strony
-    setEqualHeight();
-
-    // Uruchom funkcję również przy zmianie rozmiaru okna
-    $(window).on('resize', function() {
         setEqualHeight();
     });
     $(function() {
