0d7e309310834dd73b73b12b8c65c2e9fccfcd3a
Creastv <creastv@gail.com>
2024-11-05 09:05:07 +0100

    seo

--- functions.php
+++ functions.php
@@ -172,34 +172,9 @@ CHANGE SLUGS OF CUSTOM POST TYPES
 
 function change_post_types_slug($args, $post_type)
 {
-
-    /*item post type slug*/
     if ('offer' === $post_type) {
         $args['rewrite']['slug'] = 'oferta';
     }
-
     return $args;
 }
 add_filter('register_post_type_args', 'change_post_types_slug', 10, 2);
-
-
-function THEME_SLUG_posts_add_rewrite_rules($wp_rewrite)
-{
-    $new_rules = [
-        'a/page/([0-9]{1,})/?$' => 'index.php?post_type=post&paged=' . $wp_rewrite->preg_index(1),
-        'a/(.+?)/?$' => 'index.php?post_type=post&name=' . $wp_rewrite->preg_index(1),
-    ];
-    $wp_rewrite->rules = $new_rules + $wp_rewrite->rules;
-    return $wp_rewrite->rules;
-}
-add_action('generate_rewrite_rules', 'THEME_SLUG_posts_add_rewrite_rules');
-
-function THEME_SLUG_posts_change_blog_links($post_link, $id = 0)
-{
-    $post = get_post($id);
-    if (is_object($post) && $post->post_type == 'post') {
-        return home_url('/a/' . $post->post_name . '/');
-    }
-    return $post_link;
-}
-add_filter('post_link', 'THEME_SLUG_posts_change_blog_links', 1, 3);
